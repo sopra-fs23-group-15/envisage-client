8b50c04dd37dd8aab1fc8fe15ff4cdb371972844
armsp <shantam.raj@uzh.ch>
2023-03-19 20:32:44 +0100

    commenting unused import

--- src/components/views/Header.js
+++ src/components/views/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {ReactLogo} from "components/ui/ReactLogo";
+// import {ReactLogo} from "components/ui/ReactLogo";
 import PropTypes from "prop-types";
 import "styles/views/Header.scss";
 
