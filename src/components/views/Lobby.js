4e32097fdcbb07dbcdff61aae8d2856fd721e983
moritzmohn <moritz.mohn@uzh.ch>
2023-04-12 15:29:31 +0200

    add dummy button to start the game

--- src/components/views/Lobby.js
+++ src/components/views/Lobby.js
@@ -1,22 +1,23 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { api } from "helpers/api";
 import {useHistory} from 'react-router-dom';
 import { useLocation } from "react-router-dom";
+import { Button } from "components/ui/Button";
 
 
 const Lobby = (props) => {
     const history = useHistory();
     const location = useLocation();
-    const [lobby, setLobby()] = useState(null);
+    const [lobby, setLobby] = useState(null);
     const [users, setUsers] = useState(null);
 
     useEffect(() => {
-        const response = await api.get("/lobbies/" + location.state.lobby);
+        const response = api.get("/lobbies/" + location.state.lobby);
         setLobby(response.data);
         setUsers(lobby.players);
 
         const timer = setInterval(() => {
-            const response = await api.get("/lobbies/" + location.state.lobby);
+            const response = api.get("/lobbies/" + location.state.lobby);
             setLobby(response.data);
             setUsers(lobby.players);
         }, 8000);
@@ -24,6 +25,7 @@ const Lobby = (props) => {
     }, []);
 
     let content = <div>no users</div>
+    let content2 = <div></div>
 
     if (users) {
         content = (<ul className="game user-list">
@@ -35,7 +37,15 @@ const Lobby = (props) => {
                     </ul>)
     }
 
-    return <p>{content}</p>;
+    if (localStorage.getItem('creator') == 'true'){
+        content2 = (
+            <Button disabled={users.length < 3}>
+                Start the game
+            </Button>
+        )
+    }
+
+    return <p>{content}{content2}</p>;
 };
 
 export default Lobby;
