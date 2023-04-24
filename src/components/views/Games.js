cc2ac34d574482043a81df262421bc0212eac4e6
Marion Andermatt <marion.andermatt@gmx.ch>
2023-04-24 10:19:52 +0200

    try fix deployed version

--- src/components/views/Games.js
+++ src/components/views/Games.js
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { Button } from "components/ui/Button";
 import { Timer } from "components/ui/Timer";
 import { useParams } from "react-router-dom";
-import TextArea from "./TextArea";
+//import TextArea from "./TextArea";
 import "styles/views/Game.scss";
 
 const Games = () => {
