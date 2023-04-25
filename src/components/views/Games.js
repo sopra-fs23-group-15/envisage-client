f85b2455b260038e2b7448c5d8d88198723a7586
armsp <shantam.raj@uzh.ch>
2023-04-25 17:20:51 +0200

    TextArea component moved, added timer expire api call Some big changes - 1. Moved the TextArea component here itself since making the child component modify the parent state would require lifting the state. It is unnecessarily complicated. 2. Added implementation such that when Timer expires, it automatically submits the user input, empty or otherwise
    What needs to be edited -
    1. Since the API call to get the image is not yet finalized on server, the API calls are made to the temporary endpoint. Just need to change that and everything should just work out of the box. The request body also sends all the relevant information - player, prompt, lobbyId

--- src/components/views/Games.js
+++ src/components/views/Games.js
@@ -1,18 +1,38 @@
 import React, { useEffect, useState } from "react";
-import { handleError } from "helpers/api";
+import { api, handleError } from "helpers/api";
 import { useNavigate } from "react-router-dom";
 import { Button } from "components/ui/Button";
 import { Timer } from "components/ui/Timer";
 import { useParams } from "react-router-dom";
-import TextArea from "./TextArea";
+// import TextArea from "./TextArea";
 import "styles/views/Game.scss";
+const MAX_CHARS = 400;
 
 const Games = () => {
   const navigate = useNavigate();
   const [image, setImage] = useState(null);
+  const [prompt, setPrompt] = useState("");
   // const [keywords, setKeywords] = useState(null);
   const { lobbyId, roundId } = useParams();
+  const [text, setText] = useState("");
+  const [charCount, setCharCount] = useState(0);
 
+  const handleInputChange = (e) => {
+    const inputText = e.target.value;
+    const inputCharCount = inputText.length;
+    if (inputCharCount > MAX_CHARS) {
+      return;
+    }
+    setText(inputText);
+    setCharCount(inputCharCount);
+  };
+
+  const inputStyle = {
+    border: charCount > MAX_CHARS ? "2px solid red" : "",
+  };
+  const handlePromptChange = (value) => {
+    setPrompt(value);
+  };
   useEffect(() => {
     async function fetchImage() {
       try {
@@ -37,18 +57,13 @@ const Games = () => {
     console.log(localStorage.getItem("image"));
   }, []);
 
-  const submitPrompt = async () => {
+  const submitPrompt = async (prompt) => {
+    console.log("user prompt is: "+ prompt);
     try {
-      // const requestBody = JSON.stringify({ keywords });
-      // await api.put(
-      //   "/lobbies/" +
-      //     lobbyId +
-      //     "/games/" +
-      //     roundId +
-      //     localStorage.getItem("player"),
-      //   requestBody
-      // );
-
+      const requestBody = JSON.stringify({ "prompt": prompt, "player": localStorage.getItem("player"), "lobbyId": localStorage.getItem("lobbyId") });
+      console.log(requestBody);
+      await api.post("/getdalleimage", requestBody);
+      //code to sleep for 5 seconds...
       navigate(`/lobbies/${lobbyId}/games/${roundId}/votePage`);
     } catch (error) {
       console.error(
@@ -65,7 +80,7 @@ const Games = () => {
     <div className="game">
       <img className="game image" src={image} alt="" />
       <div className="game input">
-        <Timer func={submitPrompt} />
+        <Timer func={()=>submitPrompt(text)} />
         <div className="game input-style">
           {localStorage.getItem("challengeStyle")}
         </div>        
@@ -74,8 +89,17 @@ const Games = () => {
           placeholder="tweak your keywords to make it more fun!"
           // onChange={(kw) => setKeywords(kw)}
         /> */}
-        <TextArea />
-        <Button className="G" onClick={() => submitPrompt()}>
+        <>
+    <label className="game input-label">Describe your image. {charCount}/{MAX_CHARS}</label>
+      <textarea
+        className= "game input-field"
+        style={inputStyle}
+        value={text}
+        onChange={handleInputChange}
+        placeholder="tweak your keywords to make it more fun! (max 400 characters)"
+      />
+    </>
+        <Button className="G" onClick={() => submitPrompt(text) }>
           Submit
         </Button>
       </div>
