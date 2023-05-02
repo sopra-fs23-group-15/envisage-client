fc49d1f02e1d0762c64915776fbb4e309b8e2839
armsp <shantam.raj@uzh.ch>
2023-05-02 16:23:59 +0200

    Implemented functionality to show all user images
    backend API is still missing for this to be tested. Need to be integrated once backend is ready.
    The button in FinalPage navigates here. There was no need to trigger the get images for a user api from the button call.
    Once this view is navigated to, it automatically calls the API from useEffect.

--- src/components/views/ExhibitionPage.js
+++ src/components/views/ExhibitionPage.js
@@ -1,20 +1,65 @@
 import { useNavigate } from "react-router-dom";
 import LobbyContainer from "components/ui/LobbyContainer";
 import { Button } from "components/ui/Button";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Spinner } from "components/ui/Spinner";
+import ImageComponent from "./Image";
+
 import "styles/views/Exhibition.scss";
+import { api } from "helpers/api";
 
 const ExhibitionPage = () => {
-  const navigate = useNavigate();
+  const [imgs, setImgs] = useState([]);
 
+  const navigate = useNavigate();
   const goMain = async () => {
     localStorage.removeItem("lobbyId");
     navigate("landingPage");
   };
+  const lobbyId = localStorage.getItem("lobbyId");
+  const username = localStorage.getItem("player");
+  useEffect(()=>{
+    try{
+    async function fetch() {
+      const response = await api.get(`/lobbies/${lobbyId}/games/images/${username}`);
+    setImgs(response.data);
+  };
+    fetch();
+    
+  } catch (error){
+    return(
+      <>
+      Not implemented yet in the backend.\n
+      {error}
+      </>
+    )
+  }
+  });
+
+  let imageList = (
+    <>
+      <Spinner backgroundImage={localStorage.getItem("challengeImage")} />
+    </>
+  );
+  if (imgs) {
+    imageList =   
+    <div>
+    {imgs.map((image) => (
+           <ImageComponent
+             url={true}
+             image={image.image}
+           />
+      ))}
+    </div>
+    }
+
 
   return (
     <LobbyContainer>
       <h1>Welcome to the exhibition</h1>
+      <div>
+      {imageList}
+      </div>
       <div className="buttons">
         <Button onClick={() => goMain()}>Logout</Button>
         <Button className="button2">Restart</Button>
