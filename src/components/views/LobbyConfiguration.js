ac663cb2a9a5d9912a8a85296b9ed5add4434544
nikita-uzh <nikitaammini.amitabh@uzh.ch>
2023-05-21 14:02:01 +0200

    removed checks in configg methods (not needed anymore with button disabling) in LobbyConfiguration.js

--- src/components/views/LobbyConfiguration.js
+++ src/components/views/LobbyConfiguration.js
@@ -83,51 +83,27 @@ const LobbyConfiguration = () => {
   };
 
   const decreaseRounds = () => {
-    if (currentRounds === 0) {
-      setCurrentRounds(options_rounds.length - 1);
-    } else {
-      setCurrentRounds(currentRounds - 1);
-    }
+      setCurrentRounds(currentRounds-1);
   };
 
   const increaseRounds = () => {
-    if (currentRounds === options_rounds.length - 1) {
-      setCurrentRounds(0);
-    } else {
-      setCurrentRounds(currentRounds + 1);
-    }
+      setCurrentRounds(currentRounds+1);
   };
 
   const decreaseCategory = () => {
-    if (currentCategory === 0) {
-      setCurrentCategory(options_category.length - 1);
-    } else {
-      setCurrentCategory(currentCategory - 1);
-    }
+      setCurrentCategory(currentCategory-1);
   };
 
   const increaseCategory = () => {
-    if (currentCategory === options_category.length - 1) {
-      setCurrentCategory(0);
-    } else {
-      setCurrentCategory(currentCategory + 1);
-    }
+      setCurrentCategory(currentCategory+1);
   };
 
   const decreaseTimer = () => {
-    if (currentTimer === min_time) {
-      setCurrentTimer(max_time);
-    } else {
-      setCurrentTimer(currentTimer - interval);
-    }
+      setCurrentTimer(currentTimer-interval);
   };
 
   const increaseTimer = () => {
-    if (currentTimer === max_time) {
-      setCurrentTimer(min_time);
-    } else {
       setCurrentTimer(currentTimer + interval);
-    }
   };
 
   return (
@@ -150,7 +126,7 @@ const LobbyConfiguration = () => {
           <div> {options_category[currentCategory]} </div>
           <Button
             className="L R"
-            disabled={currentCategory === options_category.length - 1}
+            disabled={currentCategory === options_category.length-1}
             onClick={() => increaseCategory()}
           >
             ❯
@@ -170,7 +146,7 @@ const LobbyConfiguration = () => {
           <div> {options_rounds[currentRounds]} </div>
           <Button
             className="L R"
-            disabled={currentRounds === options_rounds.length - 1}
+            disabled={currentRounds === options_rounds.length-1}
             onClick={() => increaseRounds()}
           >
             +
