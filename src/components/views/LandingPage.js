2e4e98211609ff40de0f414e7c55ad191e3a91fd
Xue Wang <xue.sharon.wang@outlook.com>
2023-05-23 20:32:12 +0200

    fixed alert message

--- src/components/views/LandingPage.js
+++ src/components/views/LandingPage.js
@@ -20,13 +20,20 @@ const NumberInput = (props) => {
     if (inputValue === "" || regex.test(inputValue)) {
       setInputValue(inputValue);
       props.onChange(inputValue);
+      // reset the alert to an empty div
+      setAlert(<div className="alertMsg"></div>);
     } else {
-      setAlert(<Alert className="alertMsg" severity="error">Please enter only numbers.</Alert>);
+      setAlert(
+        <Alert className="alertMsg" severity="error">
+          Please enter only numbers.
+        </Alert>
+      );
     }
   };
 
   return (
     <div className="login field">
+      {alert}
       <label className="login label">{props.label}</label>
       <input
         type="text"
@@ -64,7 +71,7 @@ const LandingPage = () => {
   const [userName, setUsername] = useState("");
   const [lobbyId, setLobbyId] = useState("");
   const navigate = useNavigate();
-  let [alert, setAlert] = useState(<div className="adduser alert"></div>)
+  let [alert, setAlert] = useState(<div className="adduser alert"></div>);
 
   useEffect(() => {
     if (isConnected()) {
@@ -75,7 +82,6 @@ const LandingPage = () => {
     setLobbyId(pin);
   };
   const addUser = async () => {
-
     try {
       const requestBody = JSON.stringify({ userName });
       console.log(requestBody);
@@ -84,9 +90,19 @@ const LandingPage = () => {
       console.log("Connected Lobbies: " + isConnected());
       localStorage.setItem("userName", userName);
       localStorage.setItem("lobbyId", parseInt(lobbyId));
+      // reset the Alert to an empty div
+      setAlert(<div className="alertMsg"></div>);
       navigate(`/lobbies/${lobbyId}`);
     } catch (error) {
-      setAlert(<Alert className="adduser alert" severity="error">Something went wrong when joining the lobby: \n${handleError(error)}></Alert>);
+      setAlert(
+        <Alert
+          className="adduser alert"
+          severity="error"
+        // here is probably where the problem was, the whole thing was not put in {} and thus was not recognized as java code before
+        >{`Something went wrong when joining the lobby: \n${handleError(
+          error
+        )}`}</Alert>
+      );
     }
   };
 
@@ -122,9 +138,8 @@ const LandingPage = () => {
           </Button>
         </div>
       </div>
-      <p className="login manifesto">
-        How you would draw this in the game : ⇨
-      </p>
+      <p className="login manifesto">How you would draw this in the game : ⇨</p>
+      {alert}
     </BaseContainer>
   );
 };
